ca954d02bc14d83366c4f37bf988da669101af8e
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-09-17 22:15:25 +0530

    structure done

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
